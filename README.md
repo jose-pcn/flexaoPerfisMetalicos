d969262d3c8c741ae95af1740c95157ada0a6521
Jose <neto973156@gmail.com>
2024-01-20 22:04:50 -0300

    Upload Arquivo

---
+++ README.md
@@ -0,0 +1,3 @@
+> Esse projeto foi desenvolvido como parte do curso "Python para Engenharia Estrutural" do Professor Ricardo Corsini.
+
+> Consiste em uma rotina para dimensionamento à flexão de perfis metálicos laminados a frio.
---
+++ funcoes_auxiliares.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def table_steel(product):
+
+    path = './tabelas.xlsx'
+    dataFrame = pd.read_excel(path, product)
+
+    return dataFrame
+
+def param_perfil(product, bitola):
+
+    dataFrame = table_steel(product)
+    filtro = bitola
+    dfSelecionado = dataFrame.loc[dataFrame['Bitola (mm x kg/m)'] == filtro]
+    listaParam = dfSelecionado.iloc[0].to_list()
+
+    return listaParam
+#print(param_perfil('Laminados', 'W 150 x 13,0')) 
+
+def lista_bitolas(product):
+
+    dataFrame = table_steel(product)
+    listaBitolas = dataFrame['Bitola (mm x kg/m)'].to_list()
+
+    return listaBitolas
+
+print(lista_bitolas('Laminados'))
